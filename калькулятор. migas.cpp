d758445d8e15ae023bd7d1f435b3d71c93d99877
Polala1999 <32450659+Polala1999@users.noreply.github.com>
2017-10-10 20:37:06 +0300

    Initial commit

---
+++ калькулятор. migas.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a =34;
+	int b =163;
+	cout << "a = ";
+	cin >> a; cout << a << endl;
+	cout << "b = ";
+	cin >> b; cout << b << endl;
+	int c = a + b;
+	cout << "Сумма = " << c << endl; 
+		int d =-10;
+		int e =17;
+		cout << "d = ";
+		cin >> d; cout << d << endl;
+		cout << "e = ";
+		cin >> e; cout << e << endl;
+		int f = d - e;
+		cout << "Разность = " << f << endl;
+			int g =45;
+			int h =23;
+			cout << "g = ";
+			cin >> g; cout << g << endl;
+			cout << "h = ";
+			cin >> h; cout << h << endl;
+			int j = g * h;
+			cout << "Умножение = " << j << endl;
+    return 0;
+}
